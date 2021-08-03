8089a5560c39eb46c7d8df7abfa418fd98090d8c
cloudbrownie <andynguyen.aln@gmail.com>
2021-08-03 16:59:13 -0400

    Base scripts for visual effects

---
+++ scripts/vfx.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Effect:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
---
+++ scripts/vfxhandler.py
@@ -0,0 +1,10 @@
+import pygame
+
+from .vfx import Effect
+
+class VFXHandler:
+    def __init__(self):
+        pass
+
+    def handleEffects(self):
+        pass
